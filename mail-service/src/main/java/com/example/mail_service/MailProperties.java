ac6dd0ff40ea261f39586cf6b676e334a637f9d7
stahiomar <omarstahi2002@gmail.com>
2025-01-18 07:55:52 +0100

    refresh scope

---
+++ mail-service/src/main/java/com/example/mail_service/MailProperties.java
@@ -0,0 +1,67 @@
+package com.example.mail_service;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@Component
+@RefreshScope
+@ConfigurationProperties(prefix = "spring.mail")
+public class MailProperties {
+
+    private String host;
+    private int port;
+    private String username;
+    private String password;
+    private boolean smtpAuth;
+    private boolean starttlsEnable;
+
+    // Getters and Setters
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isSmtpAuth() {
+        return smtpAuth;
+    }
+
+    public void setSmtpAuth(boolean smtpAuth) {
+        this.smtpAuth = smtpAuth;
+    }
+
+    public boolean isStarttlsEnable() {
+        return starttlsEnable;
+    }
+
+    public void setStarttlsEnable(boolean starttlsEnable) {
+        this.starttlsEnable = starttlsEnable;
+    }
+}
